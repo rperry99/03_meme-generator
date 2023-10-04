b497631bc801dde8c582e7ff9ec798901c8495c8
Russ Perry <rperry1886@gmail.com>
2023-10-04 17:25:21 -0400

    Add: Using State, generate a random image when button is clicked.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,29 @@
+import { useState } from 'react';
+import memeData from '../memeData';
 import '../component-styles/form-styles.css';
 
 function Form() {
+  const [meme, setMeme] = useState();
+  function getNewMemeImage() {
+    const randomMemeIndex = Math.floor(
+      Math.random() * memeData.data.memes.length
+    );
+    const randomMeme = memeData.data.memes[randomMemeIndex].url;
+    console.log(randomMeme);
+    setMeme(randomMeme);
+  }
+
   return (
-    <section className="form">
+    <div className="form">
       <div className="textbox-container">
         <input type="text" placeholder="Top Line" />
         <input type="text" placeholder="Bottom Line" />
       </div>
-      <input type="submit" value="Get a new meme image 🖼️" className="submit" />
-    </section>
+      <button className="new-image" onClick={getNewMemeImage} type="button">
+        Get a new meme image 🖼️
+      </button>
+      <img src={meme} alt="" />
+    </div>
   );
 }
 
