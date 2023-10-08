2fae177b3ba3f93e844797ea412e9d93ed82c8c8
Russ Perry <rperry1886@gmail.com>
2023-10-08 11:51:36 -0400

    Add functionality to top and bottom text inputs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -25,16 +25,38 @@ function Form() {
     }));
   }
 
+  function handleTopChange(e) {
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      topText: e.target.value,
+    }));
+  }
+
+  function handleBottomChange(e) {
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      bottomText: e.target.value,
+    }));
+  }
+
   return (
     <div className="form">
       <div className="textbox-container">
-        <input type="text" placeholder="Top Line" />
-        <input type="text" placeholder="Bottom Line" />
+        <input type="text" placeholder="Top Line" onChange={handleTopChange} />
+        <input
+          type="text"
+          placeholder="Bottom Line"
+          onChange={handleBottomChange}
+        />
       </div>
       <button className="new-image" onClick={getNewMemeImage} type="button">
         Get a new meme image 🖼️
       </button>
-      <img src={meme.memeImage} alt="" />
+      <div className="image-container">
+        <p className="top-text meme-text">{meme.topText}</p>
+        <p className="bottom-text meme-text">{meme.bottomText}</p>
+        <img src={meme.memeImage} alt="" />
+      </div>
     </div>
   );
 }
